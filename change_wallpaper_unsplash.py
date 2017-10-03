081afc6370515676008f3c228223052987dbc06d
yamara <yklsga@gmail.com>
2017-10-03 19:23:56 -0400

    ignore images in gitignore

--- change_wallpaper_unsplash.py
+++ change_wallpaper_unsplash.py
@@ -8,26 +8,35 @@ BASE_URL = 'https://source.unsplash.com/'
 USER_URL = BASE_URL+'user/'
 COLLECTION_URL = BASE_URL + 'collection/'
 
+WIDTH = '1600'
+HEIGHT = '900'
+
 def parse_args():
     parser = argparse.ArgumentParser(description = 'arguments of how to change wallpaper')
     parser.add_argument('-d','--display', type = int, default = 0,
-                        help = 'Desktop display number on OS X (0: all displays, 1: main display, etc') 
+                        help = 'Desktop display number on OS X (0: all displays, 1: main display, etc')
+    parser.add_argument('-w','--width',type = str,default = WIDTH, help = 'width of the photo you need')
+    parser.add_argument('-e','--height',type = str,default = HEIGHT, help = 'height of the photo you need')
     subparsers = parser.add_subparsers(help = 'mode of result fixed or random or photo_id', dest = 'command')
     parser_random = subparsers.add_parser('random')
-    parser_random.add_argument('A',type = str)
+    #parser_random.add_argument('-r','--random',action = 'store_true', default = False, help = 'random flag')
 
-    parser_fixed = subparsers.add_parser('fixed')
+    parser_user = subparsers.add_parser('user')
     #parser_fixed.add_argument('-t','--time', type = str.lower, choices = ['daily','weekly'], help = 'time period')
 
 
     parser_specific_photo = subparsers.add_parser('specific_photo')
-    parser_specific_photo.add_argument('photo_id',type = str, help = 'id of the photo')
-    parser_specific_photo.add_argument('-w','--width',type = str, default = '1600',help = 'width of the photo you need')
-    parser_specific_photo.add_argument('-e','--height',type = str, default = '900',help = 'height of the photo you need')
+    parser_specific_photo.add_argument('photo_id',type=str, help='id of the photo')
+    
     return parser.parse_args()
 
-def random_image():
-    pass
+def random_image(**kwargs):
+    print('random image is called')
+    if kwargs.get('width') or kwargs.get('height'):
+        photo_response = get_response(BASE_URL+'random/'+kwargs.get('width')+'x'+kwargs.get('height'))
+    else:
+        photo_response = get_response(BASE_URL+'random')
+    save_photo(photo_response)
 
 def debug_request(r):
     print('status code = ',r.status_code)
@@ -40,8 +49,11 @@ def get_response(url):
         sys.exit("Couldn't find that photo")
     return r
 
-def specific_photo(photo_id, display, width = '1600', height = '900'):
-    photo_response = get_response(BASE_URL+photo_id+'/'+width+'x'+height)
+def specific_photo(photo_id, display, **kwargs):
+    if kwargs.get('width') or kwargs.get('height'):
+        photo_response = get_response(BASE_URL+photo_id+kwargs.get('width',WIDTH)+'x'+kwargs.get('height',HEIGHT))
+    else:
+        photo_response = get_response(BASE_URL+photo_id)
     save_photo(photo_response)
     change_wallpaper(os.getcwd()+r'/wallpaper.jpg', display)
 
@@ -77,6 +89,9 @@ def main():
     #print(args)
     if args.command == 'specific_photo':
         specific_photo(args.photo_id, args.display, width = args.width, height = args.height)
-
+    elif args.command == 'random':
+        random_image(width = args.width, height = args.height)
+        
+    
 if __name__ == '__main__':
     main()
