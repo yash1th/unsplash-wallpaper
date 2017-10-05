902c4e1a85c5799afa687c5e4e9867abaaf81f39
yamara <yklsga@gmail.com>
2017-10-05 10:47:13 -0400

    fixed pylint errors

--- unsplash_wallpaper.py
+++ unsplash_wallpaper.py
@@ -1,9 +1,13 @@
-import requests
+'''
+abcd
+'''
 import argparse
 import sys
 import os
 import platform
 import random
+import ctypes
+import requests
 
 BASE_URL = 'https://source.unsplash.com/'
 USER_URL = BASE_URL+'user/'
@@ -15,79 +19,90 @@ HEIGHT = '900'
 CATEGORY_LIST = ['nature', 'people']
 CATEGORY_NAME = random.choice(CATEGORY_LIST).lower()
 
-WALLPAPER_NAME = ''
-
 def parse_args():
-    parser=argparse.ArgumentParser(description='change wallpaper of your desktop environment using images from "unsource.unsplash.com"')
-    subparsers=parser.add_subparsers(help='type of photo you want', dest='command')
-    parser_unsplash=subparsers.add_parser('unsplash',
+    '''
+    parses the arguments
+    '''
+    parser = argparse.ArgumentParser(description='change wallpaper of your desktop environment using images from "unsource.unsplash.com"')
+    subparsers = parser.add_subparsers(help='type of photo you want', dest='command')
+    parser_unsplash = subparsers.add_parser('unsplash',
                                             description='gives a random image in entire unsplash')
     parser_unsplash.add_argument('time', nargs='?', type=str.lower, default=None,
                                  choices=['daily', 'weekly'], help='timeframe like daily or weekly')
-    parser_unsplash.add_argument('-w','--width', type=str, default =WIDTH, help='width of the photo you want')
-    parser_unsplash.add_argument('-e','--height', type=str, default=HEIGHT, help='height of the photo you want')
-    
-
-    parser_user=subparsers.add_parser('user')
-    parser_user.add_argument('username',type=str,help='username of the user')
-    parser_user.add_argument('time',nargs='?',type=str.lower,default=None,choices=['daily','weekly'],help='fixed image of the time frame')
-    parser_user.add_argument('-w','--width',type=str,default=WIDTH, help='width of the photo you need')
+    parser_unsplash.add_argument('-w', '--width', type=str, default=WIDTH, help='width of the photo you want')
+    parser_unsplash.add_argument('-e', '--height', type=str, default=HEIGHT, help='height of the photo you want')
+    #parser user
+    parser_user = subparsers.add_parser('user')
+    parser_user.add_argument('username', type=str, help='username of the user')
+    parser_user.add_argument('time', nargs='?', type=str.lower, default=None, choices=['daily', 'weekly'], help='fixed image of the time frame')
+    parser_user.add_argument('-w', '--width', type=str, default=WIDTH, help='width of the photo you need')
     parser_user.add_argument('-e', '--height', type=str, default=HEIGHT, help='height of the photo you need')
-
+    #parser for user likes
     parser_likes = subparsers.add_parser('likes')
     parser_likes.add_argument('username', type=str, help='username of the user')
     parser_likes.add_argument('-w', '--width', type=str, default=WIDTH, help='width of the photo you need')
     parser_likes.add_argument('-e', '--height', type=str, default=HEIGHT, help='height of the photo you need')
-
+    #parser for collections
     parser_collection = subparsers.add_parser('collection')
     parser_collection.add_argument('collection_id', type=str, help='ID of the collection')
     parser_collection.add_argument('-w', '--width', type=str, default=WIDTH, help='width of the photo you need')
     parser_collection.add_argument('-e', '--height', type=str, default=HEIGHT, help='height of the photo you need')
-
+    #parser for search by category
     parser_search_category = subparsers.add_parser('search_category', description='subparser to search photo by category', help='search a photo by category')
-    parser_search_category.add_argument('category',type=str.lower,nargs='?',default=CATEGORY_NAME,help='name of the category you want to search in')
-    parser_search_category.add_argument('-f',action='store_true',default=False,help='filters by searching in curated collections')
-    parser_search_category.add_argument('-time',type=str.lower,default='',choices=['daily','weekly'],help='fixed image of the time frame')
-    parser_search_category.add_argument('-w','--width',type=str,default=WIDTH, help='width of the photo you need')
-    parser_search_category.add_argument('-e','--height',type=str,default=HEIGHT, help='height of the photo you need')
-    parser_search_category.add_argument('-terms',nargs='+',default='')
-
-    parser_search_size=subparsers.add_parser('search_size', description='subparser to search photo by size', help='search a photo by given size')
-    parser_search_size.add_argument('-f',action='store_true',default=False,help='filters by searching in curated collections')
-    parser_search_size.add_argument('-time',type=str.lower,default='',choices=['daily','weekly'],help='fixed image of the time frame')
-    parser_search_size.add_argument('-w','--width',type=str,default=WIDTH, help='width of the photo you need')
-    parser_search_size.add_argument('-e','--height',type=str,default=HEIGHT, help='height of the photo you need')
-    parser_search_size.add_argument('-terms',nargs='+',default='')
-
-    parser_photo=subparsers.add_parser('photo')
-    parser_photo.add_argument('photo_id',type=str, help='id of the photo')
-    parser_photo.add_argument('-w','--width',type=str,default=WIDTH, help='width of the photo you need')
-    parser_photo.add_argument('-e','--height',type=str,default=HEIGHT, help='height of the photo you need')
-
-
-    parser.add_argument('-d','--display', type=int, default=0,
+    parser_search_category.add_argument('category', type=str.lower, nargs='?', default=CATEGORY_NAME, help='name of the category you want to search in')
+    parser_search_category.add_argument('-f', action='store_true', default=False, help='filters by searching in curated collections')
+    parser_search_category.add_argument('-time', type=str.lower, default='', choices=['daily', 'weekly'], help='fixed image of the time frame')
+    parser_search_category.add_argument('-w', '--width', type=str, default=WIDTH, help='width of the photo you need')
+    parser_search_category.add_argument('-e', '--height', type=str, default=HEIGHT, help='height of the photo you need')
+    parser_search_category.add_argument('-terms', nargs='+', default='')
+    #parser for search by size
+    parser_search_size = subparsers.add_parser('search_size', description='subparser to search photo by size', help='search a photo by given size')
+    parser_search_size.add_argument('-f', action='store_true', default=False, help='filters by searching in curated collections')
+    parser_search_size.add_argument('-time', type=str.lower, default='', choices=['daily', 'weekly'], help='fixed image of the time frame')
+    parser_search_size.add_argument('-w', '--width', type=str, default=WIDTH, help='width of the photo you need')
+    parser_search_size.add_argument('-e', '--height', type=str, default=HEIGHT, help='height of the photo you need')
+    parser_search_size.add_argument('-terms', nargs='+', default='')
+    #parser for a specific photo
+    parser_photo = subparsers.add_parser('photo')
+    parser_photo.add_argument('photo_id', type=str, help='id of the photo')
+    parser_photo.add_argument('-w', '--width', type=str, default=WIDTH, help='width of the photo you need')
+    parser_photo.add_argument('-e', '--height', type=str, default=HEIGHT, help='height of the photo you need')
+    parser.add_argument('-d', '--display', type=int, default=0,
                         help='Desktop display number on OS X (0: all displays, 1: main display, etc')
-    
     return parser.parse_args()
 
 def get_response(url):
-    r=requests.get(url)
-    if r.status_code != 200 or r.url == 'https://images.unsplash.com/photo-1446704477871-62a4972035cd?fit=crop&fm=jpg&h=800&q=50&w=1200':
+    '''
+    provides response
+    '''
+    response = requests.get(url)
+    if response.status_code != 200 or response.url == 'https://images.unsplash.com/photo-1446704477871-62a4972035cd?fit=crop&fm=jpg&h=800&q=50&w=1200':
         sys.exit("Couldn't find that photo")
-    return r
+    return response
 
 def name():
+    '''
+    provides a name
+    '''
     import datetime
-    now=datetime.datetime.now()
-    return '-'.join(map(str,[now.month,now.day,now.year]))+'_'+'.'.join(map(str,[now.hour, now.minute, now.second]))+'.jpg'
+    now = datetime.datetime.now()
+    return '-'.join(map(str, [now.month, now.day, now.year]))+'_'+'.'.join(map(str, [now.hour, now.minute, now.second]))+'.jpg'
+
+WALLPAPER_NAME = name()
 
 def save_photo(photo_response, filename):
-    with open(filename, 'wb') as f:
+    '''
+    saves photo
+    '''
+    with open(filename, 'wb') as photo:
         for chunk in photo_response:
-            f.write(chunk)
+            photo.write(chunk)
 
 def change_wallpaper(save_location, display):
-    platform_name=platform.system()
+    '''
+    changes photo
+    '''
+    platform_name = platform.system()
     if platform_name.startswith('Win'):
         #Python 3.x
         if sys.version_info >= (3, 0):
@@ -95,10 +110,9 @@ def change_wallpaper(save_location, display):
         #Python 2.x
         else:
             ctypes.windll.user32.SystemParametersInfoA(20, 0, save_location, 3)
-            
     if platform_name.startswith('Darwin'):
         if display == 0:
-            cmd="""
+            cmd = """
                             osascript -e 'tell application "System Events"
                                 set desktopCount to count of desktops
                                 repeat with desktopNumber from 1 to desktopCount
@@ -109,28 +123,32 @@ def change_wallpaper(save_location, display):
                             end tell'
                             """.format(save_location=save_location)
         else:
-            cmd="""osascript -e 'tell application "System Events"
+            cmd = """osascript -e 'tell application "System Events"
                                         set desktopCount to count of desktops
                                         tell desktop {display}
                                             set picture to "{save_location}"
                                         end tell
-                                    end tell'""".format(display=display, save_location=save_location)
-        
+                                    end tell'""".format(display=display, save_location=save_location)        
         os.system(cmd)
 
 def get_image(url):
-    photo_response=get_response(url)
-    global WALLPAPER_NAME
-    WALLPAPER_NAME=name()
+    '''
+    gets the image from internet
+    '''
+    photo_response = get_response(url)
     save_photo(photo_response, WALLPAPER_NAME)
 
 def default():
+    '''
+    in case of no arguments at all
+    '''
     get_image(BASE_URL+'featured/'+WIDTH+'x'+HEIGHT+'/?')
 
 def main():
-    import time
-    s=time.time()
-    args=parse_args()
+    '''
+    main function
+    '''
+    args = parse_args()
     if args.command is None:
         default()
     if args.command == 'unsplash':
@@ -145,24 +163,20 @@ def main():
             get_image(USER_URL+args.username+'/'+args.time)
     elif args.command == 'likes':
         get_image(USER_URL+args.username+'/likes/'+args.width+'x'+args.height)
-    
     elif args.command == 'collection':
         get_image(COLLECTION_URL+args.collection_id+'/'+args.width+'x'+args.height)
-    
     elif args.command == 'search_category':
-        if args.f:    
+        if args.f:
             get_image(BASE_URL+'category/'+args.category+'/featured/'+args.width+'x'+args.height+'/'+args.time+'?'+','.join(args.terms))
         else:
             get_image(BASE_URL+'category/'+args.category+'/'+args.width+'x'+args.height+'/'+args.time+'?'+','.join(args.terms))
     elif args.command == 'search_size':
-        if args.f:    
+        if args.f:
             get_image(BASE_URL+'featured/'+args.width+'x'+args.height+'/'+args.time+'?'+','.join(args.terms))
         else:
             get_image(BASE_URL+'/'+args.width+'x'+args.height+'/'+args.time+'?'+','.join(args.terms))
-
     elif args.command == 'photo':
         get_image(BASE_URL+args.photo_id+'/'+args.width+'x'+args.height)
-
     change_wallpaper(os.getcwd()+r'/'+WALLPAPER_NAME, args.display)
 
 
